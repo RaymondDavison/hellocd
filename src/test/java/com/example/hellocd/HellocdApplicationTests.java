efcf787bcf779533df16544c84ed5ee53183db44
RaymondDavison <aakomonehraymond@yahoo.com>
2022-05-23 10:54:55 +0000

    adding files created wiht spring boot generator

---
+++ src/test/java/com/example/hellocd/HellocdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellocd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellocdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
